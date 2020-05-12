fb334b386bb19a38568a888a9b4a79a8bfef081d
Noah Krever <ndk2115@columbia.edu>
2020-05-12 04:49:48 -0400

    structural fixes, scrolling links

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-function scrollSmoothToBottom (id) {
-    var div = document.getElementById(id);
-    $('#' + id).animate({
-       scrollTop: div.scrollHeight - div.clientHeight
-    }, 500);
- }
+
 function showAll() {
     document.getElementById('math').style.display = 'block';
     document.getElementById('stats').style.display = 'block';
